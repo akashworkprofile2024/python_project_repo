9e035e31720f86a4ea0e647211af8661170a8605
akashworkprofile2024 <akashbiswas.officialmail2023@gmail.com>
2025-08-30 10:20:44 +0530

    some minor changes in interface python file

---
+++ Banking_System/main.py
@@ -0,0 +1,17 @@
+import subprocess  # run Another script in one go means python file run in another python file
+
+def main():
+    print('Welcome To Neo Bank')
+    print('1. Register Account')
+    print('2. Login Account')
+    print('3. Admin Account')
+    
+    choice = input("Enter your choice (1 or 2): ")
+
+    if choice == '1':
+        subprocess.run(['python3', 'register_account.py'])    
+    elif choice == '2':
+        subprocess.run(['python3', 'Client_login.py'])
+    else:
+        print('Invalid choice')       
+main()
